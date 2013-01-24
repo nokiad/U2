6a2dc6a06722046236197553bb7e07406aecc16e
深津惇斗 <shinat2c@yahoo.co.jp>
2013-01-24 11:26:43 +0900

    プログラミング応用Aの課題２
    ユニティゲーム

---
+++ Assets/NewBehaviourScript.js
@@ -0,0 +1,13 @@
+#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	transform.position.x +=
+		Input.GetAxis("Horizontal") * (50.0 * Time.deltaTime);
+	transform.position.z +=
+		Input.GetAxis("Vertical")	* (50.0 * Time.deltaTime);
+
+}
